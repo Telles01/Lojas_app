0008f8d2912655fd23a7b0ff7d3559882a763753
Bernard Teles <bernardtelgon@gmail.com>
2024-04-15 17:08:06 -0300

    building a db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_15_194447) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_15_195722) do
   create_table "lojas", force: :cascade do |t|
     t.string "name"
     t.integer "stars"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
