4f17e22783b1f0b6c425f4b5818e768ba8fdcd88
Bernard Teles <bernardtelgon@gmail.com>
2024-04-16 16:17:31 -0300

    building a crud

--- app/controllers/lojas_controller.rb
+++ app/controllers/lojas_controller.rb
@@ -3,4 +3,36 @@ class LojasController < ApplicationController
   def index
     @lojas = Loja.all
   end
+
+  def show
+    @loja = Loja.find(params[:id])
+  end
+
+  def new
+    @loja = Loja.new
+  end
+
+  def create
+    @loja = Loja.new(loja_params)
+    @loja.save
+    redirect_to loja_path(@loja)
+  end
+
+  def edit
+    @loja = Loja.find(params[:id])
+  end
+
+  def update
+
+  end
+
+private
+
+def loja_params
+  params.require(:loja).permit(:name, :address, :stars)
+
+end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   #list all lojas
   get 'lojas', to: 'lojas#index'
+
+  #show form to create a new loja
+  get 'lojas/new', to: 'lojas#new'
+
+  #list one loja
+  get 'lojas/:id', to: 'lojas#show', as: 'loja'
+
+  #create a new loja
+  post 'lojas', to: 'lojas#create'
+
+  #show form to edit a loja
+  get 'lojas/:id/edit', to: 'lojas#edit'
+
+  #update a loja
+  patch 'lojas/:id', to: 'lojas#update'
 
 end
